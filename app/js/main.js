52c30282d743905eaf8d7a96261ae3055f1eaddf
Dieter Raber <me@dieterraber.net>
2015-08-25 22:23:07 +0200

    requirejs setup, started scss cascade

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,22 @@
 
+'use strict';
+
+requirejs.config({
+    baseUrl: 'js',
+    paths: {
+        jquery: 'vendor/jquery-2.1.1-deprecated.min',
+        text  : 'vendor/require-plugins/text',
+        json  : 'vendor/require-plugins/json'
+    }
+});
+
+
+require([
+	'jquery', 
+	'json!config/themes.json'
+
+	], 
+	function($, themes) {
+
+    console.log( themes) 
+})
