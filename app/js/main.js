9f20c9b72584d59167f129d8f58ddac31240091c
Dieter Raber <me@dieterraber.net>
2015-08-27 22:27:45 +0200

    Theming now working

--- app/js/main.js
+++ app/js/main.js
@@ -6,17 +6,30 @@ requirejs.config({
     paths: {
         jquery: 'vendor/jquery-2.1.1-deprecated.min',
         text  : 'vendor/require-plugins/text',
-        json  : 'vendor/require-plugins/json'
+        json  : 'vendor/require-plugins/json',
+        themeManager: 'theme-manager'
     }
 });
 
 
 require([
-	'jquery', 
-	'json!config/themes.json'
-
+	'themeManager'
 	], 
-	function($, themes) {
+	function(themeManager) {
 
-    console.log( themes) 
-})
+        themeManager.init();
+
+        // just for fun
+        var boxes = [document.querySelector('.c'), document.querySelector('.a')];
+        var themes = themeManager.getAll();
+        var current = 0;
+        window.setInterval(function() {
+            current++;
+            if(current > 1){
+                current = 0;
+            }
+            var theme = themes[Math.floor(Math.random() * themes.length)];
+            themeManager.set(theme, boxes[current]);
+
+        }, 3000);
+});
