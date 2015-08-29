e0cd382bb65891d6e4cf178aef1d11449e5050fd
Dieter Raber <me@dieterraber.net>
2015-08-29 18:50:03 +0200

    changed HTML semantics

--- app/js/main.js
+++ app/js/main.js
@@ -19,17 +19,17 @@ require([
 
         themeManager.init();
 
-        // just for fun
-        var boxes = [document.querySelector('.c'), document.querySelector('.a')];
-        var themes = themeManager.getAll();
-        var current = 0;
-        window.setInterval(function() {
-            current++;
-            if(current > 1){
-                current = 0;
-            }
-            var theme = themes[Math.floor(Math.random() * themes.length)];
-            themeManager.set(theme, boxes[current]);
+        // // just for fun
+        // var boxes = [document.querySelector('.c'), document.querySelector('.a')];
+        // var themes = themeManager.getAll();
+        // var current = 0;
+        // window.setInterval(function() {
+        //     current++;
+        //     if(current > 1){
+        //         current = 0;
+        //     }
+        //     var theme = themes[Math.floor(Math.random() * themes.length)];
+        //     themeManager.set(theme, boxes[current]);
 
-        }, 3000);
+        // }, 3000);
 });
